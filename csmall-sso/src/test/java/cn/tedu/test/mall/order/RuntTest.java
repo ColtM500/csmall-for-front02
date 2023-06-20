921ce8ff64241a3d0a4f130763d67cb1be3dfda5
肖旭伟 <xiaoxw@tedu.cn>
2023-06-20 23:48:48 +0800

    2.seckill 1.0 准备了初步的秒杀代码 3.调整了一些不需要的内容

--- csmall-sso/src/test/java/cn/tedu/test/mall/order/RuntTest.java
+++
@@ -1,9 +0,0 @@
-package cn.tedu.test.mall.order;
-
-public class RuntTest {
-    public static void main(String[] args) {
-        String a="asdfasdf";
-        String[] split = a.split(",");
-        System.out.println(a.split(",")[0]);
-    }
-}
