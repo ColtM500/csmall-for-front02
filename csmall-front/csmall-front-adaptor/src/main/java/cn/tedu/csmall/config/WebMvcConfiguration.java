8a5c1123dcee985c7ca467fe2bec9395e905d039
chen <602042954@qq.com>
2023-06-25 19:23:30 +0800

    12

--- csmall-front/csmall-front-adaptor/src/main/java/cn/tedu/csmall/config/WebMvcConfiguration.java
+++ csmall-front/csmall-front-adaptor/src/main/java/cn/tedu/csmall/config/WebMvcConfiguration.java
@@ -1,6 +1,23 @@
 package cn.tedu.csmall.config;
 
-public class WebMvcConfiguration {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@Slf4j
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.debug("front开启跨域");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
 }
