8c12ac9bbea9dc2588a468e70e5c84f186914d78
chen <602042954@qq.com>
2023-06-25 17:07:05 +0800

    12

---
+++ csmall-front/csmall-front-adaptor/src/main/java/cn/tedu/csmall/FrontApplication.java
@@ -0,0 +1,18 @@
+//package cn.tedu.csmall;
+//
+//import cn.tedu.mall.common.config.MallCommonConfiguration;
+//import lombok.extern.slf4j.Slf4j;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Import;
+//
+//@SpringBootApplication
+//@Slf4j
+//@MapperScan("cn.tedu.csmall.mapper")
+//@Import(MallCommonConfiguration.class)
+//public class FrontApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(FrontApplication.class, args);
+//    }
+//}
--- csmall-front/csmall-front-adaptor/src/test/java/cn/tedu/test/tedu/front/FrontApplication.java
+++ csmall-front/csmall-front-adaptor/src/test/java/cn/tedu/test/tedu/front/FrontApplication.java
@@ -1,9 +1,14 @@
 package cn.tedu.test.tedu.front;
 
+import cn.tedu.mall.common.config.MallCommonConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Slf4j
+@Import(MallCommonConfiguration.class)
 @SpringBootApplication
 @MapperScan("cn.tedu.mall.front.mapper")
 public class FrontApplication {
