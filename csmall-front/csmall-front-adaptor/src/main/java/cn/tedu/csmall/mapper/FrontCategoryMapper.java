9ba81fc31488455c59a2d741866bf510ee31cf37
chen <602042954@qq.com>
2023-06-25 17:14:12 +0800

    12

--- csmall-front/csmall-front-adaptor/src/main/java/cn/tedu/csmall/mapper/FrontCategoryMapper.java
+++ csmall-front/csmall-front-adaptor/src/main/java/cn/tedu/csmall/mapper/FrontCategoryMapper.java
@@ -4,9 +4,11 @@ import cn.tedu.mall.pojo.front.entity.FrontCategoryEntity;
 import cn.tedu.mall.pojo.front.vo.FrontCategoryTreeVO;
 import cn.tedu.mall.pojo.product.vo.CategoryStandardVO;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FrontCategoryMapper {
      @Select("select * from pms_category")
      List<FrontCategoryEntity> selectAll();
